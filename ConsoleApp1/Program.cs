98cc97ec8742ecebd9a89230f65351393b60c0f7
Adan Mariscal <Adan Mariscal@DESKTOP-AF0JERK>
2021-03-18 12:25:22 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            string helloWorldMessage = "Hello World";
+            Console.WriteLine(HelloWorld(helloWorldMessage));
+        }
+        private static string HelloWorld(string message)
+        {
+            return message;
+        }
+    }
+}
